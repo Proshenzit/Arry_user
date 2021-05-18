c0fb009b5ab4c161a379568bb4ad42daae568426
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-18 00:51:22 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,55 @@
+
+var num  = new Array();
+
+for(var i=0;i<5;i++)
+{
+
+    num[i]=parseFloat(prompt("Enter your  Number :"));
+
+}
+
+var sum=0;
+
+for(var i = 0;i < 5;i++)
+{
+    console.log(num[i]);
+
+    sum=sum+num[i];
+}
+console.log("sum="+sum);   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
